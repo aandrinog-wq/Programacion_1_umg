f56995e63507a35c0741de9227748e623896e86d
Fernando Andrino <aandrinog@miumg.edu.gt>
2026-01-28 11:57:03 -0600

    Agrego programa de c++

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hellow wordl!" << endl;
+    cout << "Axel Fernando Andrino Garcia" << endl;
+    cout << "9959-25-8978" << endl;
+    return 0;
+}
